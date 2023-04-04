c8a86c1f1f5ebcc423f1cf961b0ea81573509c21
John Choi <johnchoi1003@icloud.com>
2023-04-04 11:20:26 -0400

    Added skeleton

---
+++ src/test/java/com/github/osucoding/petplatevision/PetPlateVisionApplicationTests.java
@@ -0,0 +1,13 @@
+package com.github.osucoding.petplatevision;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PetPlateVisionApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
