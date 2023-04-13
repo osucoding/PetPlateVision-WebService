ae8b2a6d965889d7c856fae1dc65853d1319b3cf
John Choi <johnchoi1003@icloud.com>
2023-04-13 15:19:16 -0400

    Adding test cases

--- src/test/java/com/github/osucoding/petplatevision/PetPlateVisionApplicationTests.java
+++ src/test/java/com/github/osucoding/petplatevision/PetPlateVisionApplicationTests.java
@@ -3,11 +3,29 @@ package com.github.osucoding.petplatevision;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
+import java.lang.reflect.Method;
+import java.util.List;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+
 @SpringBootTest
 class PetPlateVisionApplicationTests {
 
 	@Test
 	void contextLoads() {
+	}
+
+	@Test
+	void applicationContextTest() {
+		PetPlateVisionApplication.main(new String[] {});
+	}
+
+	@Test
+	void testPackageStructureAndAppName() {
+		assertEquals("com.github.osucoding.petplatevision.PetPlateVisionApplication", PetPlateVisionApplication.class.getName());
+		List<Method> petPlateVisionApplicationMethodList = List.of(PetPlateVisionApplication.class.getMethods());
+		assertNotNull(petPlateVisionApplicationMethodList.stream().filter(m -> m.getName().equals("main")).findFirst().orElse(null));
 	}
 
 }
